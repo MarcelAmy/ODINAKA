fe9c0240ec6c3196ccf6deaeae743133a3bc6543
MarcelAmy <marcelodinakachi070@gmail.com>
2024-12-13 16:29:14 +0100

    banye

---
+++ welove/weluv/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def base(request):
+    return render(request,'base.html')
+def middle(request):
+    return render(request,'middle.html')
+def again(request):
+    return render(request,'again.html')
